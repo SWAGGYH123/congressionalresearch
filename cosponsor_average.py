cf6e79fb615d2bdbb4a224e49f88920354396a32
Hugo Chiasson <38743538+shapelessdream@users.noreply.github.com>
2023-04-16 13:02:46 -0700

    Add files via upload

---
+++ cosponsor_average.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import json
+import os
+from collections import Counter
+
+path = '/Users/Hugo/Desktop/Research/Research/Cosponsor'
+dirs = os.listdir(path)
+
+js_folder_path=os.path.join('/Users', 'Hugo', 'Desktop', 'Research', 'Research', 'Cosponsor')
+
+average111 = Counter()
+average112=Counter()
+average113=Counter()
+average114=Counter()
+average115=Counter()
+average116=Counter()
+cntr=[0]*6
+count=Counter()
+js = [x for x in dirs if x.endswith('.json')]
+for js in js:
+  js_path = os.path.join(js_folder_path, js)
+  with open(js_path,'r') as f: 
+    data = json.loads(f.read())
+    if 'request' not in data.keys(): continue
+    if data['request']['congress']=='111':
+      try:
+        cntr[0]+=1
+        for item in data['cosponsors']:
+          average111[(item['party'])] += 1
+      except KeyError as e:
+        print(js_path)
+    if data['request']['congress']=='112':
+      try:
+        cntr[1]+=1
+        for item in data['cosponsors']:
+          average112[(item['party'])] += 1
+      except KeyError as e:
+        print(js_path)
+    if data['request']['congress']=='113':
+      try:
+        cntr[2]+=1
+        for item in data['cosponsors']:
+          average113[(item['party'])] += 1
+      except KeyError as e:
+        print(js_path)
+    if data['request']['congress']=='114':
+      try:
+        cntr[3]+=1
+        for item in data['cosponsors']:
+          average114[(item['party'])] += 1
+      except KeyError as e:
+        print(js_path)
+    if data['request']['congress']=='115':
+      try:
+        cntr[4]+=1
+        for item in data['cosponsors']:
+          average115[(item['party'])] += 1
+      except KeyError as e:
+        print(js_path)
+    if data['request']['congress']=='116':
+      try:
+        cntr[5]+=1
+        for item in data['cosponsors']:
+          average116[(item['party'])] += 1
+      except KeyError as e:
+        print(js_path)
+
+print('Congress 111:')
+print(average111)
+print("Congress 112:") 
+print(average112)
+print("Congress 113:")
+print(average113)
+print("Congress 114:")
+print(average114)
+print("Congress 115:")
+print(average115)
+print("Congress 116:")
+print(average116)
+
+print(cntr)   
---
+++ counter.py
@@ -0,0 +1,45 @@
+import os
+import json
+
+DIR = os.curdir + "/Cosponsor/"
+
+path = os.listdir(DIR)
+
+def filter_jsons(x):
+    return x[-5:] == ".json"
+
+
+def filter_congress(x, i):
+    return x[:3] == i
+
+
+def bipartisan(p):
+    if (("D" in p) or ("ID" in p) or ("I" in p)) and (("R" in p) or ("L" in p)):
+        return True
+
+jsons = list(filter(filter_jsons, path))
+only_111 = list(filter(lambda x: filter_congress(x, "111"), jsons))
+only_112 = list(filter(lambda x: filter_congress(x, "112"), jsons))
+only_113 = list(filter(lambda x: filter_congress(x, "113"), jsons))
+only_114 = list(filter(lambda x: filter_congress(x, "114"), jsons))
+only_115 = list(filter(lambda x: filter_congress(x, "115"), jsons))
+only_116 = list(filter(lambda x: filter_congress(x, "116"), jsons))
+
+def check_congress(n):
+    counter = 0
+    for i in n:
+        with open(DIR + i, 'r') as x:
+            x_parsed = json.load(x)
+            parties = []
+            for j in x_parsed['cosponsors']:
+                parties.append(j['party'])
+            if bipartisan(parties):
+                counter += 1
+    return counter
+
+print("Congress 111: " + str(check_congress(only_111)))
+print("Congress 112: " + str(check_congress(only_112)))
+print("Congress 113: " + str(check_congress(only_113)))
+print("Congress 114: " + str(check_congress(only_114)))
+print("Congress 115: " + str(check_congress(only_115)))
+print("Congress 116: " + str(check_congress(only_116)))
